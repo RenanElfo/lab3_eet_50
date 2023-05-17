877c2173445c5a03a4291f62a3092a76ca85bbd1
Renan Elfo <elfoacademico@gmail.com>
2023-05-17 13:24:18 -0300

    DSB Modulation Almost Finished

--- common.py
+++ common.py
@@ -1,103 +1,85 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import fft, fftfreq, fftshift
+from scipy.fft import rfft, rfftfreq
 from scipy.io import wavfile
+
+
+class Signal:
+    data_array: np.ndarray
+    sample_rate: float
+    length: float
+
+    def __init__(self, data_array, sample_rate, length):
+        self.data_array = data_array
+        self.sample_rate = sample_rate
+        self.length = length
+
+    @property
+    def sample_number(self):
+        return int(self.sample_rate * self.length)
+
+    @property
+    def time(self):
+        return np.linspace(0., self.length, self.sample_number)
+
+    @property
+    def fourier_frequencies(self):
+        return rfftfreq(self.sample_number, 1/self.sample_rate)
+
+    @property
+    def fourier_array(self):
+        return rfft(self.data_array, norm='forward')
+
+    def plot_data(self, start_index=0, end_index=-1):
+        time = self.time[start_index:end_index]
+        data = self.data_array[start_index:end_index]
+        plt.figure()
+        plt.plot(time, data)
+        plt.xlabel('Tempo [s]')
+        plt.ylabel('Amplitude')
+        plt.show()
+
+    def plot_fourier(self, start_index=0, end_index=-1):
+        frequencies = self.fourier_frequencies[start_index:end_index]
+        amplitude = np.abs(self.fourier_array)[start_index:end_index]
+        plt.figure()
+        plt.plot(frequencies, amplitude)
+        plt.title('Absolute Value of Amplitude')
+        plt.xlabel('Frequência [Hz]')
+        plt.ylabel('Amplitude')
+        plt.show()
 
 
 class SenoidSignal:
     senoid_frequency: np.ndarray
-    sample_rate: int
-    signal_length: float
     amplitude: float
     phase: float
+    signal: Signal
 
-    def __init__(self, senoid_frequency, sample_rate, signal_length, *, amplitude=0, phase=0):
+    def __init__(self, senoid_frequency, sample_rate, length,
+                 *, amplitude=0, phase=0):
         self.senoid_frequency = senoid_frequency
-        self.sample_rate = sample_rate
-        self.signal_length = signal_length
         self.amplitude = amplitude
         self.phase = np.deg2rad(phase)
+        self.signal = self._get_signal(sample_rate, length)
 
-    @property
-    def sample_number(self):
-        return int(self.signal_length * self.sample_rate)
-
-    @property
-    def time(self):
-        return np.linspace(0., self.signal_length, self.sample_number)
-
-    @property
-    def data_array(self):
-        sine = np.sin(2*np.pi*self.senoid_frequency*self.time + self.phase)
-        return self.amplitude * sine
-
-    @property
-    def fourier_frequencies(self):
-        return fftshift(fftfreq(self.sample_number, 1/self.sample_rate))
-
-    @property
-    def fourier_array(self):
-        return fftshift(fft(self.data_array, norm='forward'))
-
-    def plot_data(self):
-        plt.figure()
-        plt.plot(self.time, self.data_array)
-        plt.xlabel('Tempo [s]')
-        plt.ylabel('Amplitude')
-        plt.show()
-
-    def plot_fourier(self, start_index=0, end_index=-1):
-        frequencies = self.fourier_frequencies[start_index:end_index]
-        amplitude = np.abs(self.fourier_array)[start_index:end_index]
-        plt.figure()
-        plt.plot(frequencies, amplitude)
-        plt.title('Absolute Value of Amplitude')
-        plt.xlabel('Frequência [Hz]')
-        plt.ylabel('Amplitude')
-        plt.show()
+    def _get_signal(self, sample_rate, length):
+        sample_number = int(sample_rate * length)
+        time = np.linspace(0., length, sample_number)
+        sine = np.sin(2*np.pi*self.senoid_frequency*time + self.phase)
+        data_array = self.amplitude * sine
+        return Signal(data_array, sample_rate, length)
 
 
 class Audio:
-    data_array: np.ndarray
-    sample_rate: int
+    signal: Signal
 
     def __init__(self, audio_path):
-        self.sample_rate, audio_stereo = wavfile.read(audio_path)
-        self.data_array = audio_stereo[:, 0]
+        self.signal = self._get_signal(audio_path)
 
-    @property
-    def sample_number(self):
-        return self.data_array.shape[0]
-
-    @property
-    def audio_length(self):
-        return self.sample_number / self.sample_rate
-
-    @property
-    def time(self):
-        return np.linspace(0., self.audio_length, self.sample_number)
-
-    @property
-    def fourier_frequencies(self):
-        return fftshift(fftfreq(self.sample_number, 1/self.sample_rate))
-
-    @property
-    def fourier_array(self):
-        return fftshift(fft(self.data_array, norm='forward'))
-
-    def plot_data(self):
-        plt.figure()
-        plt.plot(self.time, self.data_array)
-        plt.xlabel('Tempo [s]')
-        plt.ylabel('Amplitude')
-        plt.show()
-
-    def plot_fourier(self, start_index=0, end_index=-1):
-        frequencies = self.fourier_frequencies[start_index:end_index]
-        amplitude = np.abs(self.fourier_array)[start_index:end_index]
-        plt.figure()
-        plt.plot(frequencies, amplitude)
-        plt.title('Absolute Value of Amplitude')
-        plt.xlabel('Frequência [Hz]')
-        plt.ylabel('Amplitude')
-        plt.show()
+    def _get_signal(self, audio_path):
+        sample_rate, audio_stereo = wavfile.read(audio_path)
+        data_array = audio_stereo[:, 0]
+        sample_number = data_array.shape[0]
+        length = sample_number / sample_rate
+        return Signal(data_array, sample_rate, length)
--- dsb_modulation.py
+++ dsb_modulation.py
@@ -1,34 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.fft import rfft, rfftfreq
 
-from common import SenoidSignal, Audio
+from common import Signal, SenoidSignal, Audio
 
 class ModulatedSignal:
-    signal: SenoidSignal | Audio
+    sample_rate: float
+    length: float
+    modulated: Signal
+    baseband: Signal
+    modulator: SenoidSignal | Audio
     carrier: SenoidSignal
     k_a: float
 
     def __init__(self, signal, carrier, k_a):
-        self.signal = signal
+        self.modulator = signal
         self.carrier = carrier
+        self.sample_rate = self._get_sample_rate()
+        self.length = self._get_length()
         self.k_a = self._get_k_a(k_a)
+        self.modulated = self._get_signal()
+        self.baseband = self._get_bandbase()
 
     def _get_k_a(self, k_a):
         if k_a <= 0:
             raise ValueError('k_a must be positive.')
         return k_a
 
-    @property
-    def data_array(self):
-        return (1 + self.k_a*self.signal.data_array) * self.carrier.data_array
-    
-    @property
-    def time(self):
-        return self.signal.time
-    
-    def plot_data(self):
-        plt.figure()
-        plt.plot(self.time, self.data_array)
-        plt.xlabel('Tempo [s]')
-        plt.ylabel('Amplitude')
-        plt.show()
+    def _get_sample_rate(self):
+        modulator_sample_rate = self.modulator.signal.sample_rate
+        carrier_sample_rate = self.carrier.signal.sample_rate
+        if modulator_sample_rate != carrier_sample_rate:
+            message = "sinal and carrier don't have the same sample rate."
+            raise ValueError(message)
+        return modulator_sample_rate
+
+    def _get_length(self):
+        if self.modulator.signal.length != self.carrier.signal.length:
+            message = "sinal and carrier don't have the same length."
+            raise ValueError(message)
+        return self.modulator.signal.length
+
+    def _get_signal(self):
+        modulator_data_array = self.modulator.signal.data_array
+        carrier_data_array = self.carrier.signal.data_array
+        data_array = (1 + self.k_a*modulator_data_array) * carrier_data_array
+        return Signal(data_array, self.sample_rate, self.length)
+
+    def _get_bandbase(self):
+        modulator_data_array = self.modulator.signal.data_array
+        carrier_amplitude = self.carrier.amplitude
+        data_array = carrier_amplitude * (1 + self.k_a*modulator_data_array)
+        return Signal(data_array, self.sample_rate, self.length)
